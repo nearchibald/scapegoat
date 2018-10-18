d23090b9a248bfda8d566573d85a159487750352
nearchibald <tf.kozlov@gmail.com>
2018-10-19 00:18:52 +0700

    [D1] Finished analysis

--- Factor Analysis/2DAssetAllocation.py
+++ Factor Analysis/2DAssetAllocation.py
@@ -1,7 +1,7 @@
-# Exploratory Factor Analysis described here
+# Asset Allocation with PCA (Offence and Defence portfolios)
 
-# https://cssanalytics.wordpress.com/2018/07/23/2d-asset-allocation-using-pca-part-1/
-# https://cssanalytics.wordpress.com/2018/08/21/2d-asset-allocation-using-pca-part-2/
+# ref #1 https://cssanalytics.wordpress.com/2018/07/23/2d-asset-allocation-using-pca-part-1/
+# ref #2 https://cssanalytics.wordpress.com/2018/08/21/2d-asset-allocation-using-pca-part-2/
 
 import sys, os
 sys.path.append("...//")
@@ -12,32 +12,68 @@ from DataLoader import *
 from matplotlib import pyplot as plt
 
 # setup the data
-Data = Data(tickers = ["IVV", "EFA", "IEFA", "AGG"])
+Data = Data()
 Data.load_data()
 Data = Data.get_data()
 
-# Steps:
-# 0) make a returns
-# 1) check the distribution by principal components (weights)
-# 2) check the first component's composition  for PC1
-# 3)
+# todo: make a class
+# todo: add example
+# todo: add full_report func
 
+### start ref #1
+
+# make returns and normilize data
+
+Data = Data.diff().dropna()
 data_normalized = pd.DataFrame(preprocessing.StandardScaler().fit_transform(Data))
 data_normalized.columns = Data.columns
+
 # plt.plot(data_normalized)
 # plt.show()
 
-fa = decomposition.PCA(n_components=2)
-principalComponents = fa.fit_transform(data_normalized)
+fa = decomposition.PCA() # setup PCA
+principalComponents = fa.fit_transform(data_normalized) # run PCA
 
-principalDf = pd.DataFrame(principalComponents)
-print(principalDf)
-plt.plot(principalDf)
+principalDf = pd.DataFrame(principalComponents) # principal components time seires
+
+# plt.plot(principalDf.iloc[:, :3]) # print first 3 components
+# plt.show()
+
+# explained variance of components
+print("Explained variance by component: %s" %fa.explained_variance_ratio_)
+# print(pd.DataFrame(fa.components_)) # prints weights
+first_comp = pd.Series(pd.DataFrame(fa.components_).iloc[0, :]) # select the first component
+first_comp.index = list(Data.columns)
+first_comp = first_comp.sort_values()
+
+# first_comp.plot(kind="bar") # plots ordered weights of the first component
+# plt.show()
+
+### finish ref #1
+
+### start ref #2
+
+# setup weights of Offence and Defence portfolios
+Offense = first_comp[first_comp > 0]
+Offense = Offense / Offense.sum()
+Defense = first_comp[first_comp < 0]
+Defense = Defense / Defense.sum()
+
+# setup portfolio
+Def_equity = Data.loc[:, list(Defense.index)].multiply(Defense).sum(axis=1)
+Off_equity = Data.loc[:, list(Offense.index)].multiply(Offense).sum(axis=1)
+EqualWeights = Data.sum(axis = 1)
+EqualWeights = EqualWeights / len(list(Data.columns))
+EqualWeights.iloc[0] = 1
+Def_equity.iloc[0] = 1
+Off_equity.iloc[0] = 1
+
+Portfolios = pd.DataFrame({"Offence": Off_equity.cumsum().values,
+                           "Defense": Def_equity.cumsum().values,
+                           "Equal_Weights": EqualWeights.cumsum().values}, index=list(Off_equity.index))
+
+# print Offence, Defence and Equal Weights portfolios
+Portfolios.plot(legend=True)
 plt.show()
 
-
-print("Explained variance by component: %s" %fa.explained_variance_ratio_)
-print(pd.DataFrame(fa.components_))
-
-# http://emmanuel-klinger.net/factor-analysis-principal-component-analysis-and-scikit-learn.html
-# https://www.youtube.com/watch?time_continue=323&v=kApPBm1YsqU
+### finish ref #2
